579d9d469fa27f690a68870d66dd9a394ae98349
JUVING <129962308+JUVING@users.noreply.github.com>
2024-03-11 03:06:03 +0900

    Add files via upload

---
+++ CHAPTER 12/CreateGraphicsApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace CHAPTER_12
+{
+    public partial class CreateGraphicsApp : Form
+    {
+        public CreateGraphicsApp()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateGraphicsApp_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Random r = new Random();
+            Color c = Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+            g.FillRectangle(new SolidBrush(c), ClientRectangle);
+        }
+    }
+}
---
+++ CHAPTER 12/RectContainsApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CHAPTER_12
+{
+    class RectContainsApp
+    {
+        /*public static void Main()
+        {
+            Rectangle rect = new Rectangle(10, 20, 80, 40);
+            Point pt1 = new Point(30, 40);
+            Point pt2 = new Point(90, 60);
+            Console.WriteLine("포함되는가?:" + $"{rect.Contains(pt1)}");
+            Console.WriteLine("포함되는가?:" + $"{rect.Contains(pt2)}");
+        }*/
+    }
+}
